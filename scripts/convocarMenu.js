4d8d4c7a3675805aa39f9481261f020016ab7842
aaron <aaron@daw.com>
2024-01-18 15:13:15 +0000

    Curriculum vite

--- scripts/convocarMenu.js
+++
@@ -1,6 +0,0 @@
-fetch("menu_superior.html")
-  .then((response) => response.text())
-  .then((data) => {
-    const contenedor = document.querySelector("#contenedor-menu-superior");
-    contenedor.innerHTML = data;
-  });
--- scripts/toggleProyectoInfo.js
+++
@@ -1,5 +0,0 @@
-function toggleProyectoInfo(button) {
-    const proyectoInfo = button.parentNode.parentNode;
-    proyectoInfo.classList.toggle('proyecto-expand');
-  }
-  
